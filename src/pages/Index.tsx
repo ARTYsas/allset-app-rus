7a456d898111ad17b920fe2257b3a5809ebc1e49
ARTYsas <128177662+ARTYsas@users.noreply.github.com>
2025-05-16 09:28:59 +0500

    test commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,8 @@
 
 import { Navigate } from 'react-router-dom';
 
+<h1 className="text-2xl text-green-600">Обновление от 16 мая — работает 🎉</h1>
+
 const Index = () => {
   return <Navigate to="/" replace />;
 };
